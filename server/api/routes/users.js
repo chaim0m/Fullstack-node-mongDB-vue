94815919e01f753eba1b2af77fbb1719e735e626
Michael Finson <32094844+chaim0m@users.noreply.github.com>
2019-02-13 11:30:41 +0200

    Update users.js

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
 async function loadUsersCollection() {
   const client = await mongodb.MongoClient.connect(
-    'mongodb://chaim0m:tz314459090@cluster0-shard-00-00-hwhan.mongodb.net:27017,cluster0-shard-00-01-hwhan.mongodb.net:27017,cluster0-shard-00-02-hwhan.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    'your mongoDB connection string',
     {
       useNewUrlParser: true
     }
