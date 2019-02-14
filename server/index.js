18bc23cc6a824376efb2b71e0e47bb5b098a67d4
Unknown <chaimfinson@gmail.com>
2019-02-14 14:13:29 +0200

    Added support for development
    Hopefully

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ app.use(cors());
 
 const users = require('./api/routes/users');
 app.use('/api/users', users);
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+    // Static folder
+    app.use(express.static(__dirname + '/public/'))
+
+    //Handle SPA
+    app.get(/.*/), (req,res) => res.sendFile(__dirname + '/public/index.html')
+}
+
 
 const port = process.env.PORT || 4000;
 
