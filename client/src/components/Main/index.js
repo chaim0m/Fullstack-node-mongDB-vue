18749f4ca80e3ab64efb293cfef854409c2799db
Unknown <chaimfinson@gmail.com>
2019-02-24 13:06:34 +0200

    small fix for url on FE

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -2,7 +2,7 @@ import { compose, withData, withProps, withHandlers } from 'vue-compose';
 import axios from "axios";
 import Main from './Main.vue';
 
-const url = 'api/users'
+const url = 'http://localhost:4000/api/users'
 
 export default compose(
     withData({
@@ -20,7 +20,7 @@ export default compose(
         return test;
       },
       async fetchList() {
-        const result = await axios.get('url');
+        const result = await axios.get(url);
         this.$emit('List', result.data);  
     }
     }))
